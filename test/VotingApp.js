1844a74b620d4c0a40b9c96757da897650284616
Heyzel <heyzelm7@gmail.com>
2022-02-15 00:28:38 -0400

    create file for testing

---
+++ test/VotingApp.js
@@ -0,0 +1,19 @@
+const {expect} = require('chai');
+
+describe('App contract', () => {
+    let App, app, owner;
+
+    beforeEach(async () => {
+        App = await ethers.getContractFactory('VotingApp');
+        app = await App.deploy();
+        owner = await ethers.getSigners();
+    });
+
+    describe('Deployment', () => {
+        it('Should set the right owner', async () => {
+            expect(await app.owner()).to.equal(owner.address);
+        });
+    });
+
+
+});
